04f85d1bf3c2eedad0a6cab2f766d1517cb3619e
bin <tinnt0504@gmail.com>
2024-07-27 10:43:55 +0700

    fix icon

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -2,7 +2,7 @@ function checkScrollPosition() {
   var w = window.innerWidth;
   var buttons = document.getElementById("float-buttons");
   var icons = document.getElementById("float-icons");
-  if (w > 426) {
+  if (w > 769) {
     if (window.scrollY <= 500) {
       buttons.style.display = "none";
       icons.style.display = "flex";
@@ -12,7 +12,7 @@ function checkScrollPosition() {
     }
   } else {
     buttons.style.display = "none";
-    icons.style.display = "flex";
+    icons.style.display = "none";
   }
 }
 
