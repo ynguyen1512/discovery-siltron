cd592ffdb24364c0d5deaffde9235b834b57ea1b
bin <tinnt0504@gmail.com>
2024-07-31 21:28:50 +0700

    fix social animation

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -2,13 +2,21 @@ function checkScrollPosition() {
   var w = window.innerWidth;
   var buttons = document.getElementById("float-buttons");
   var icons = document.getElementById("float-icons");
+  var scrollTopbtn = document.getElementById("scroll-top-button");
   if (w > 769) {
+    console.log("Scroll", window.scrollY);
     if (window.scrollY <= 500) {
       buttons.style.display = "none";
       icons.style.display = "flex";
     } else {
       icons.style.display = "none";
       buttons.style.display = "flex";
+    }
+
+    if(window.scrollY >= 4000) {
+      scrollTopbtn.style.display = "block";
+    } else {
+      scrollTopbtn.style.display = "none";
     }
   } else {
     buttons.style.display = "none";
